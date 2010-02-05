c8409658933a4fe03fdc29522e9754d561ce3562
Aaron Bedra <aaron@aaronbedra.com>
2010-02-05 13:57:38 -0500

    updating the path to enlightenment, adding a repl runner, and adding the about vectors koans.

--- koans/path_to_enlightenment.clj
+++ koans/path_to_enlightenment.clj
@@ -1,4 +1,5 @@
-(load "about-asserts")
+(load "about_asserts")
+(load "about_vectors")
 
 (defmacro meditate-on
   "Runs tests but exits if an assertion fails"
@@ -6,12 +7,18 @@
   `(try
    (~test-function)
    (catch Throwable t#
-     (println (str (:zen (meta #'~test-function)) "\n"
-                   (:name (meta #'~test-function))
-                   " returned " t#))
+     (println (str "\n\"" (:zen (meta #'~test-function)) "\"\n"
+                   "[FAILURE] - "(:name (meta #'~test-function))
+                   " failed\n" t# "\n"))
      (System/exit 1))))
 
 (meditate-on test-truth)
 (meditate-on test-assert-equality)
 (meditate-on test-fill-in-values)
+
+(meditate-on test-creating-vectors)
+(meditate-on test-vector-literals)
+(meditate-on test-accessing-vector-elements)
+(meditate-on test-vectors-and-ranges)
+
 (println "You have achieved clojure zen. Go out into the universe and spread your newfound knowledge.")
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject functional-koans "0.0.1"
   :description "The functional koans"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [jline "0.9.94"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
   :namespaces [koans])
