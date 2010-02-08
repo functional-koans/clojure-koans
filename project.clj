b02095fd7f1133b9b531d259811d97aa6df5719f
Eric Lavigne <lavigne.eric@gmail.com>
2010-02-08 10:20:49 +0800

    Added leiningen-run to the project. Now you can run it with: lein run

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject functional-koans "0.0.1"
   :description "The functional koans"
+  :script "koans/path_to_enlightenment.clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [jline "0.9.94"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [leiningen-run "0.3"]]
   :namespaces [koans])
