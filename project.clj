19a16f201f786573ccd06bac0a26078a226f44fe
Colin Jones <trptcolin@gmail.com>
2010-07-12 18:13:19 -0500

    not really using a main

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject functional-koans "0.0.1"
   :description "The functional koans"
-  :main "path_to_enlightenment"
   :dependencies [[org.clojure/clojure "1.1.0"]]
   :dev-dependencies [[leiningen-run "0.3"]]
   :namespaces [koans])
