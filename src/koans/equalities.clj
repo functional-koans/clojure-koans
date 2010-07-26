927212f25360619cdffc748052037344f74dab6a
Colin Jones <trptcolin@gmail.com>
2010-07-26 18:20:42 -0500

    standardizing indentation

--- src/koans/equalities.clj
+++ src/koans/equalities.clj
@@ -1,9 +1,9 @@
 (meditations
- "We shall contemplate truth by testing reality, via equality."
- (= __ true)
+  "We shall contemplate truth by testing reality, via equality."
+  (= __ true)
 
- "To understand reality, we must compare our expectations against reality."
- (= __ (+ 1 1))
+  "To understand reality, we must compare our expectations against reality."
+  (= __ (+ 1 1))
 
- "You can test equality of many things"
- (= (+ 3 4) __ (+ 2 __)))
+  "You can test equality of many things"
+  (= (+ 3 4) __ (+ 2 __)))
