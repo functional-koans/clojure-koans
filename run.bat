c38fcd124756647fbc3eb1d3af60cbd2eaa1039f
Colin Jones <trptcolin@gmail.com>
2010-07-27 21:21:16 -0500

    fixes for windows launchers

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-java -cp lib/clojure-1.2.0-beta1.jar;src clojure.main -e '(load "path_to_enlightenment")' -e "(do (in-ns 'path-to-enlightenment) (run))"
+@echo off
+java -cp lib\clojure-1.2.0-beta1.jar;src clojure.main -e "(load \"path_to_enlightenment\")" -e "(do (in-ns 'path-to-enlightenment) (run))"
