e3ff13b84e11bd3da29ed896c38606e860c7351f
Colin Jones <trptcolin@gmail.com>
2010-08-08 20:13:26 -0400

    updating to RC2

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-java -cp lib/clojure-1.2.0-RC1.jar:src clojure.main -e '(load "path_to_answer_sheet")' -e "(do (in-ns 'path-to-answer-sheet) (run))"
+java -cp lib/clojure-1.2.0-RC2.jar:src clojure.main -e '(load "path_to_answer_sheet")' -e "(do (in-ns 'path-to-answer-sheet) (run))"
 echo
