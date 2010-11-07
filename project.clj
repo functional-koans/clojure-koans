e10fc56c2c07588e18bc3eca2dac3cadb4dead58
Colin Jones <trptcolin@gmail.com>
2010-11-07 10:38:04 -0600

    Update to 1.3.0-alpha3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject functional-koans "0.1.0"
   :description "The functional koans"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha2"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha3"]
                  [jline "0.9.94"]])
