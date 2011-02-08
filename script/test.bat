08415ecb947a84e445d82236935b29f2b1a22551
Jaskirat <me@jaskirat.info>
2011-02-08 17:26:47 +0530

    The scripts\test.bat did not work in windows because src was not on the classpath, made change to add src to the classpath

--- script/test.bat
+++ script/test.bat
@@ -5,5 +5,5 @@ for /R ./lib %%a in (*.jar) do (
    set CLASSPATH=!CLASSPATH!;%%a
 )
 set CLASSPATH=!CLASSPATH!"
-
+set CLASSPATH=%CLASSPATH%;src
 java -Xmx1G -cp "%CLASSPATH%" clojure.main -i script\test.clj
