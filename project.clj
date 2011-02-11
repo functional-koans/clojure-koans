37ab2e4e2d1f3204b9adf3f153f82e3d8a9d5152
Colin Jones <trptcolin@gmail.com>
2011-02-10 21:13:51 -0600

    Increment version to 0.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject functional-koans "0.2.0"
+(defproject functional-koans "0.3.0"
   :description "The functional koans"
   :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
                  [jline "0.9.94"]])
