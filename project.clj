312332d94e43bbb972db7b9395a9e17bef63d1fe
Colin Jones <trptcolin@gmail.com>
2011-04-09 17:30:58 -0500

    Exclude unnecessary junit dependency of jline.
    This has gone on long enough.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject functional-koans "0.3.0"
   :description "The functional koans"
   :dependencies [[org.clojure/clojure "1.3.0-alpha6"]
-                 [jline "0.9.94"]])
+                 [jline "0.9.94" :exclusions [junit]]])
