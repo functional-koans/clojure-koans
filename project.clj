d224979625adc0ecbe0a3d58745c481e3e87730f
Colin Jones <trptcolin@gmail.com>
2011-04-09 17:31:39 -0500

    Bump version to 0.3.6.
    Warning: these version numbers are pretty much arbitrary.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject functional-koans "0.3.0"
+(defproject functional-koans "0.3.6"
   :description "The functional koans"
   :dependencies [[org.clojure/clojure "1.3.0-alpha6"]
                  [jline "0.9.94" :exclusions [junit]]])
