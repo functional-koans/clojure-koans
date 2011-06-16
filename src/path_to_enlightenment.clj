8e33a43ee89c225609f93e7225edcb3477d4ab1e
Colin Jones <trptcolin@gmail.com>
2011-06-16 17:47:28 -0500

    Catch exceptions that happen before the assertion can be made

--- src/path_to_enlightenment.clj
+++ src/path_to_enlightenment.clj
@@ -6,10 +6,19 @@
 (def __ :fill-in-the-blank)
 (def ___ (fn [& args] __))
 
+(defmacro fancy-assert
+  ([x] (fancy-assert x ""))
+  ([x message]
+   `(try
+     (assert ~x ~message)
+     (catch Exception e#
+       (throw (Exception. (str '~message "\n" '~x )
+                          e#))))))
+
 (defmacro meditations [& forms]
   (let [pairs (partition 2 forms)
         tests (map (fn [[doc# code#]]
-                     `(assert ~code# ~doc#))
+                     `(fancy-assert ~code# ~doc#))
                    pairs)]
     `(do ~@tests)))
 
