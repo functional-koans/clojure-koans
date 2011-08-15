b8c529d421f5e7a7b8f6971470322596ed463435
Colin Jones <trptcolin@gmail.com>
2011-08-14 19:51:20 -0500

    Update fresh dependency to exclude hidden files
    Fixes #15

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject functional-koans "0.4.1"
+(defproject functional-koans "0.4.2"
   :description "The functional koans"
   :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
-                 [fresh "1.0.1"]
+                 [fresh "1.0.2"]
                  [jline "0.9.94" :exclusions [junit]]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public"})
