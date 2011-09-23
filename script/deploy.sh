c2d99c3d97ba37227cc0a7d93ef4d9adf46dac4d
Colin Jones <trptcolin@gmail.com>
2011-09-23 15:01:06 -0500

    Tweak deploy script to allow for multiple zipfiles per day

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 git push
-zip -r clojure-koans-`date +"%Y-%m-%d"`.zip . -x "./.git/*"
+zip -r clojure-koans-`date +"%Y-%m-%d_%H-%M"`.zip . -x "./.git/*"
 
 echo
 echo "Don't forget to upload the zipfile!"
-echo `ls -tr clojure-koans-*.zip | head -n1`
+echo `ls -t clojure-koans-*.zip | head -n1`
