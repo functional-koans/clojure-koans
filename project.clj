1cc6a8422245dab061db9b2f683430391f70630d
Colin Jones <trptcolin@gmail.com>
2011-09-23 15:11:01 -0500

    Bump version for stack overflow bugfix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject functional-koans "0.4.3"
+(defproject functional-koans "0.4.4"
   :description "The functional koans"
   :dependencies [[org.clojure/clojure "1.3.0-RC0"]
                  [fresh "1.0.2"]
