9c2fd12c58413c1c8ef1f352c38d1ed48e45c41f
Colin Jones <trptcolin@gmail.com>
2011-09-24 13:15:26 -0500

    Update to Clojure 1.3.0 final

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject functional-koans "0.4.4"
+(defproject functional-koans "0.4.5"
   :description "The functional koans"
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [fresh "1.0.2"]
                  [jline "0.9.94" :exclusions [junit]]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]])
