e2b5f87984f71ea90b1b81789a5c6aa59a89cd30
Colin Jones <trptcolin@gmail.com>
2011-09-28 14:00:38 -0500

    Add note about auto-uploading the zipfile

--- script/deploy.sh
+++ script/deploy.sh
@@ -12,3 +12,8 @@ echo "Don't forget to upload the zipfile"
 echo "  to https://github.com/functional-koans/clojure-koans/downloads"
 echo `ls -t releases/clojure-koans-*.zip | head -n1`
 echo
+
+# TODO: use http://developer.github.com/v3/repos/downloads/
+#       to do the upload automatically
+#       GET example:
+#         curl https://api.github.com/repos/functional-koans/clojure-koans/downloads
