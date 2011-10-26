977e9985926c769c12f1911523a9c3d212d7df74
Aaron Bedra <aaron@aaronbedra.com>
2011-10-25 22:39:13 -0400

    Don't rely on SNAPSHOT version of swank-clojure

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [fresh "1.0.2"]
                  [jline "0.9.94" :exclusions [junit]]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]])
+  :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]])
