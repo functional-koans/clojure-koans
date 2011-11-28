8bdf7015a563e16e12d77027564132129edc55d3
Colin Jones <trptcolin@gmail.com>
2011-11-27 18:10:03 -0600

    Update engine dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject functional-koans "0.4.5"
   :description "The functional koans."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [koan-engine "0.1.0"]]
+                 [koan-engine "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]
                      [lein-koan "0.1.0"]])
