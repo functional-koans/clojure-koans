7c00277b17636be8da4adb7a0b721fe86fb96d11
Colin Jones <trptcolin@gmail.com>
2011-11-28 19:17:41 -0600

    Update to snapshot

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject functional-koans "0.4.5"
   :description "The functional koans."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [koan-engine "0.1.1"]]
+                 [koan-engine "0.1.2-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]
                      [lein-koan "0.1.0"]])
