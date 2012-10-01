1cfa85cf571720f479adf68399814ed53ead708b
Colin Jones <trptcolin@gmail.com>
2012-09-30 22:05:49 -0500

    Bump koan-engine
    refs #29

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojure-koans "0.4.5"
+(defproject clojure-koans "0.4.6-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [koan-engine "0.1.2-SNAPSHOT"]]
+                 [koan-engine "0.1.2"]]
   :dev-dependencies [[lein-koan "0.1.2"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.2"]]}}
   :plugins [[lein-koan "0.1.2"]]
