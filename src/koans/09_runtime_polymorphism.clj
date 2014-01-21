0cf4cd3121fc7bae7d545064e80ef157e3fe8d5c
Colin Jones <trptcolin@gmail.com>
2014-01-21 07:14:00 -0600

    Move herbivore assertions together

--- src/koans/09_runtime_polymorphism.clj
+++ src/koans/09_runtime_polymorphism.clj
@@ -26,13 +26,13 @@
   (= "Bambi eats veggies."
      (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
 
-  "Different methods are used depending on the dispatch function result"
-  (= "Simba eats animals."
-     (diet {:species "lion" :name "Simba" :age 1 :eater :carnivore}))
-
   "Animals have different names"
   (= "Thumper eats veggies."
     (diet {:species "rabbit" :name "Thumper" :age 1 :eater :herbivore}))
+
+  "Different methods are used depending on the dispatch function result"
+  (= "Simba eats animals."
+     (diet {:species "lion" :name "Simba" :age 1 :eater :carnivore}))
 
   "You may use a default method when no others match"
   (= "I don't know what Rich Hickey eats."
