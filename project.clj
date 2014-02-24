c7bd27047a82a84327cf6b581bee7bda01794110
Colin Jones <trptcolin@gmail.com>
2014-02-23 21:13:11 -0600

    Bump koan-engine for doc/code alternation check

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-koans "0.5.0-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [koan-engine "0.2.0"]]
+                 [koan-engine "0.2.1"]]
   :dev-dependencies [[lein-koan "0.1.2"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.2"]]}}
   :repl-options {:init-ns user}
