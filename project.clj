b987ae9e5aab2f636f26e686eb85147018101e9a
Colin Jones <trptcolin@gmail.com>
2014-06-13 14:34:38 -0500

    Add random runner

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject clojure-koans "0.5.0-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [koan-engine "0.2.2"]]
-  :dev-dependencies [[lein-koan "0.1.2"]]
-  :profiles {:dev {:dependencies [[lein-koan "0.1.2"]]}}
+                 [koan-engine "0.2.3"]]
+  :dev-dependencies [[lein-koan "0.1.3"]]
+  :profiles {:dev {:dependencies [[lein-koan "0.1.3"]]}}
   :repl-options {:init-ns koan-engine.runner
                  :init (use 'koan-engine.core)}
-  :plugins [[lein-koan "0.1.2"]]
+  :plugins [[lein-koan "0.1.3"]]
   :main koan-engine.runner/exec)
