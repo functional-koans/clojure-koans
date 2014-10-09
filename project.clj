2c524896c9c71020141467c334cc59c0aef67531
Colin Jones <trptcolin@gmail.com>
2014-10-09 08:34:13 -0500

    Bump to SNAPSHOT for next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-koans "0.5.0"
+(defproject clojure-koans "0.5.1-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [koan-engine "0.2.3"]]
