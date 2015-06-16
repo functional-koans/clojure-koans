fc9367c0f4d330370bc4dbd1d7c6a03331b1afc1
Florent Delannoy <florent.delannoy@gmail.com>
2015-06-16 20:38:38 +1200

    Add Eclipse integration through lein-eclipse plugin

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [koan-engine "0.2.3"]]
   :dev-dependencies [[lein-koan "0.1.3"]]
-  :profiles {:dev {:dependencies [[lein-koan "0.1.3"]]}}
+  :profiles {:dev {:dependencies [[lein-koan "0.1.3"]
+                                  [lein-eclipse "1.0.0"]]}}
   :repl-options {:init-ns koan-engine.runner
                  :init (do (use '[koan-engine.core]))}
   :plugins [[lein-koan "0.1.3"]]
