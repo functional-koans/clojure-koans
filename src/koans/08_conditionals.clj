83aca99ae46d1567a89832f0cf4ac5be6049cd6d
SuperStevenZ <zzuohong@gmail.com>
2015-10-16 23:13:25 -0500

    My answers for it. Fixed a spelling mistake: koans.23-meta 12:18 *succintly* -> succinctly Have questions about line 13 (meta '^{:division "West"} Giants)  ;;incorrect arity 2 for meta

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -1,47 +1,47 @@
 (ns koans.08-conditionals
-  (:require [koan-engine.core :refer :all]))
+    (:require [koan-engine.core :refer :all]))
 
 (defn explain-exercise-velocity [exercise-term]
-  (case exercise-term
-        :bicycling        "pretty fast"
-        :jogging          "not super fast"
-        :walking          "not fast at all"
-        "is that even exercise?"))
+      (case exercise-term
+            :bicycling        "pretty fast"
+            :jogging          "not super fast"
+            :walking          "not fast at all"
+            "is that even exercise?"))
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
+  (= :a (if (false? (= 4 5))
           :a
           :b))
 
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3)
           []))
 
   "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
-          [:a :b :c]))
+  (= nil (if (nil? 0)
+           [:a :b :c]))
 
   "In others your alternative may be interesting"
   (= :glory (if (not (empty? ()))
               :doom
-              __))
+              :glory))
 
   "You may have a multitude of possible paths"
   (let [x 5]
-    (= :your-road (cond (= x __) :road-not-taken
-                        (= x __) :another-road-not-taken
-                        :else __)))
+       (= :your-road (cond (= x __) :road-not-taken
+                           (= x __) :another-road-not-taken
+                           :else :your-road)))
 
   "Or your fate may be sealed"
-  (= 'doom (if-not (zero? __)
-          'doom
-          'more-doom))
+  (= 'doom (if-not (zero? 1)
+                   'doom
+                   'more-doom))
 
   "In case of emergency, go fast"
   (= "pretty fast"
-     (explain-exercise-velocity __))
+     (explain-exercise-velocity :bicycling))
 
   "But admit it when you don't know what to do"
-  (= __
+  (= "is that even exercise?"
      (explain-exercise-velocity :watching-tv)))
--- src/koans/19_datatypes.clj
+++ src/koans/19_datatypes.clj
@@ -1,45 +1,48 @@
 (ns koans.19-datatypes
-  (:require [koan-engine.core :refer :all]))
+    (:require [koan-engine.core :refer :all]))
 
 (defrecord Nobel [prize])
 (deftype Pulitzer [prize])
 
 (defprotocol Award
-  (present [this recipient]))
+             (present [this recipient]))
 
 (defrecord Oscar [category]
-  Award
-  (present [this recipient]
-    (print (str "Congratulations on your "
-                (:category this) " Oscar, "
-                recipient
-                "!"))))
+           Award
+           (present [this recipient]
+                    (print (str "Congratulations on your "
+                                (:category this) " Oscar, "
+                                recipient
+                                "!"))))
 
 (deftype Razzie [category]
-  Award
-  (present [this recipient]
-    __))
+         Award
+         (present [this recipient]
+                  (print (str "You're really the "
+                              (.category this) ", "
+                              recipient
+                              "... sorry."))))
 
 (meditations
   "Holding records is meaningful only when the record is worthy of you"
-  (= __ (.prize (Nobel. "peace")))
+  (= "peace" (.prize (Nobel. "peace")))
 
   "Types are quite similar"
-  (= __ (.prize (Pulitzer. "literature")))
+  (= "literature" (.prize (Pulitzer. "literature")))
 
   "Records may be treated like maps"
-  (= __ (:prize (Nobel. "physics")))
+  (= "physics" (:prize (Nobel. "physics")))
 
   "While types may not"
-  (= __ (:prize (Pulitzer. "poetry")))
+  (= nil (:prize (Pulitzer. "poetry")))
 
   "Further study reveals why"
-  (= __
+  (= [true false]
      (map map? [(Nobel. "chemistry")
                 (Pulitzer. "music")]))
 
   "Either sort of datatype can define methods in a protocol"
-  (= __
+  (= "Congratulations on your Best Picture Oscar, Evil Alien Conquerors!"
      (with-out-str (present (Oscar. "Best Picture") "Evil Alien Conquerors")))
 
   "Surely we can implement our own by now"
