ad5c0e6081b2784947ec38ce9348e33fde88f3c2
Colin Jones <trptcolin@gmail.com>
2017-10-24 11:13:26 -0500

    Bump koan-engine to remove old jline

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-koans "0.5.1-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [koan-engine "0.2.3"]]
+                 [koan-engine "0.2.4"]]
   :dev-dependencies [[lein-koan "0.1.3"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.3"]]}}
   :repl-options {:init-ns koan-engine.runner
