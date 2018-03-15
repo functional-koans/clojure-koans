46496626ea2425004e884426eba29f24f09aee17
brannx <brannson@yandex.ru>
2018-03-15 15:32:25 +0300

    Update 05_sets.clj

--- src/koans/05_sets.clj
+++ src/koans/05_sets.clj
@@ -3,20 +3,20 @@
             [clojure.set :as set]))
 
 (meditations
-  "You can create a set by converting another collection"
+  "Создаём множество из другой коллекции"
   (= #{3} (set __))
 
-  "Counting them is like counting other collections"
+  "Считаем как обычно"
   (= __ (count #{1 2 3}))
 
-  "Remember that a set is a *mathematical* set"
+  "Множество — оно математическое"
   (= __ (set '(1 1 2 2 3 3 4 4 5 5)))
 
-  "You can ask clojure for the union of two sets"
+  "Объединение"
   (= __ (set/union #{1 2 3 4} #{2 3 5}))
 
-  "And also the intersection"
+  "Пересечение"
   (= __ (set/intersection #{1 2 3 4} #{2 3 5}))
 
-  "But don't forget about the difference"
+  "Разность"
   (= __ (set/difference #{1 2 3 4 5} #{2 3 5})))
