44ccc0ee908eaf9084512d25da91779c796c1fac
Colin Jones <trptcolin@gmail.com>
2018-12-31 09:38:13 -0600

    Bump to latest Clojure

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject clojure-koans "0.5.1-SNAPSHOT"
   :description "The Clojure koans."
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [koan-engine "0.2.4"]]
-  :dev-dependencies [[lein-koan "0.1.4"]]
-  :profiles {:dev {:dependencies [[lein-koan "0.1.4"]]}}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [koan-engine "0.2.5"]]
+  :dev-dependencies [[lein-koan "0.1.5"]]
+  :profiles {:dev {:dependencies [[lein-koan "0.1.5"]]}}
   :repl-options {:init-ns koan-engine.runner
                  :init ^:displace (do (use '[koan-engine.core]))}
-  :plugins [[lein-koan "0.1.4"]]
+  :plugins [[lein-koan "0.1.5"]]
   :main koan-engine.runner/exec)
