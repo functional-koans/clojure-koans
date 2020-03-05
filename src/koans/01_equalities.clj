f235c8431da9bf265ae0540a9c5202b5b4b26536
Juliano Lima <juliano.lima@nubank.com.br>
2020-03-05 16:00:12 -0300

    Finish First chapter

--- src/koans/01_equalities.clj
+++ src/koans/01_equalities.clj
@@ -1,39 +1,38 @@
 (ns koans.01-equalities
   (:require [koan-engine.core :refer :all]))
-
 (meditations
-  "We shall contemplate truth by testing reality, via equality"
-  (= __ true)
+ "We shall contemplate truth by testing reality, via equality"
+ (= true true)
 
-  "To understand reality, we must compare our expectations against reality"
-  (= __ (+ 1 1))
+ "To understand reality, we must compare our expectations against reality"
 
-  "You can test equality of many things"
-  (= (+ 3 4) 7 (+ 2 __))
+ (= 2 (+ 1 1))
+ "You can test equality of many things"
+ (= (+ 3 4) 7 (+ 2 5))
 
-  "Some things may appear different, but be the same"
-  (= __ (= 2 2/1))
+ "Some things may appear different, but be the same"
+ (= true (= 2 2/1))
 
-  "You cannot generally float to heavens of integers"
-  (= __ (= 2 2.0))
+ "You cannot generally float to heavens of integers"
+ (= false (= 2 2.0))
 
-  "But a looser equality is also possible"
-  (= __ (== 2.0 2))
+ "But a looser equality is also possible"
+ (= true (== 2.0 2))
 
-  "Something is not equal to nothing"
-  (= __ (not (= 1 nil)))
+ "Something is not equal to nothing"
+ (= true (not (= 1 nil)))
 
-  "Strings, and keywords, and symbols: oh my!"
-  (= __ (= "hello" :hello 'hello))
+ "Strings, and keywords, and symbols: oh my!"
+ (= false (= "hello" :hello 'hello))
 
-  "Make a keyword with your keyboard"
-  (= :hello (keyword __))
+ "Make a keyword with your keyboard"
+ (= :hello (keyword "hello"))
 
-  "Symbolism is all around us"
-  (= 'hello (symbol __))
+ "Symbolism is all around us"
+ (= 'hello (symbol 'hello))
 
-  "What could be equivalent to nothing?"
-  (= __ nil)
+ "What could be equivalent to nothing?"
+ (= nil nil)
 
-  "When things cannot be equal, they must be different"
-  (not= :fill-in-the-blank __))
+ "When things cannot be equal, they must be different"
+ (not= :fill-in-the-blank nil))
