fbe0c832d805d2384d68d60ebf03813eddaba837
pfarwick-latacora <56267801+pfarwick-latacora@users.noreply.github.com>
2021-06-15 16:47:42 -0500

    Revert " Vector and Sets clj completed"

--- src/koans/04_vectors.clj
+++ src/koans/04_vectors.clj
@@ -3,31 +3,31 @@
 
 (meditations
   "You can use vectors in clojure as array-like structures"
-  (= 1 (count [42]))
+  (= __ (count [42]))
 
   "You can create a vector from a list"
-  (= [1] (vec '(1)))
+  (= __ (vec '(1)))
 
   "Or from some elements"
-  (= [nil nil] (vector nil nil))
+  (= __ (vector nil nil))
 
   "But you can populate it with any number of elements at once"
-  (= [1 2 ] (vec '(1 2)))
+  (= [1 __] (vec '(1 2)))
 
   "Conjoining to a vector is different than to a list"
-  (= [111 222 333] (conj [111 222] 333))
+  (= __ (conj [111 222] 333))
 
   "You can get the first element of a vector like so"
-  (= :peanut (first [:peanut :butter :and :jelly]))
+  (= __ (first [:peanut :butter :and :jelly]))
 
   "And the last in a similar fashion"
-  (= :jelly (last [:peanut :butter :and :jelly]))
+  (= __ (last [:peanut :butter :and :jelly]))
 
   "Or any index if you wish"
-  (= :jelly (nth [:peanut :butter :and :jelly] 3))
+  (= __ (nth [:peanut :butter :and :jelly] 3))
 
   "You can also slice a vector"
-  (= [:butter :and](subvec [:peanut :butter :and :jelly] 1 3))
+  (= __ (subvec [:peanut :butter :and :jelly] 1 3))
 
   "Equality with collections is in terms of values"
-  (= (list 1 2 3) (vector 1 2 3)))
+  (= (list 1 2 3) (vector 1 2 __)))
