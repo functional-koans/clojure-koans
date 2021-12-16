930693ab7b35d01cded8f486754a7a9a7630b168
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2021-12-16 16:07:09 +0100

    Join last two strings

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -10,8 +10,8 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] (str (:name a), " eats ", "veggies."))
-(defmethod diet :carnivore [a] (str (:name a), " eats ", "animals."))
+(defmethod diet :herbivore [a] (str (:name a) " eats veggies."))
+(defmethod diet :carnivore [a] (str (:name a) " eats animals."))
 (defmethod diet :default [a] (str "I don't know what ", (:name a), " eats."))
 
 (meditations
