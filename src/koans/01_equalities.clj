4580850fc3ce2bf1881411e43d8284a5a32b7926
Matt Davidson <MED139@student.aru.ac.uk>
2022-11-04 13:42:32 +0000

    Finished first koan and some answers for second

--- src/koans/01_equalities.clj
+++ src/koans/01_equalities.clj
@@ -24,16 +24,17 @@
   (= true (not (= 1 nil)))
 
   "Strings, and keywords, and symbols: oh my!"
-  (= __ (= "hello" :hello 'hello))
+  (= false (= "hello" :hello 'hello)) 
 
   "Make a keyword with your keyboard"
-  (= :hello (keyword __))
+  (= :hello (keyword 'hello))
+
 
   "Symbolism is all around us"
-  (= 'hello (symbol __))
+  (= 'hello (symbol "hello"))
 
   "What could be equivalent to nothing?"
-  (= __ nil)
+  (= nil nil)
 
   "When things cannot be equal, they must be different"
-  (not= :fill-in-the-blank __))
+  (not= :fill-in-the-blank "fill-in-the-blank"))
