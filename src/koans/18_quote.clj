edaae03c53036525e02c14ac4315472d0de9cb6e
Matthew Davidson <matthewdavidson1997@gmail.com>
2023-01-04 14:38:43 +0100

    18

--- src/koans/18_quote.clj
+++ src/koans/18_quote.clj
@@ -4,13 +4,13 @@
 
 (meditations
   "Wrap a quote around a list to suppress evaluation"
-  (= (quote (1 2 3 4 5)) '(1 2 3 4 5)) 
+  (= (quote (1 2 3 4 5)) '(1 2 3 4 5))
 
   "There is a shortcut too!"
   (= (quote (1 2 3 4 5)) '(1 2 3 4 5))
 
   "You can quote symbols as well as lists... without evaluation!"
-  (= 'age (let [age 9] (quote age))) 
+  (= 'age (let [age 9] (quote age)))
 
   "You can use a literal list as a data collection without having Clojure try to call a function"
   (= (cons 1 (quote (2 3))) (list 1 2 3) (cons 1 [2 3]))
